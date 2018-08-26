505b3747c1022cd05b53956deceed6053c5b8784
okapie <tatigreuze@yahoo.co.jp>
2018-08-26 12:01:17 +0900

    Create a simple todo list.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,22 @@
+package todolist
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import spark.Spark.get
 
+data class Task(
+    val id: Long,
+    val content: String,
+    val done: Boolean
+)
+
 fun main(args: Array<String>) {
-    get("/hello") { request, response ->
-        "Hello, world!"
+    val objectMapper = ObjectMapper().registerKotlinModule()
+    get("/tasks") { request, response ->
+        val tasks = listOf(
+            Task(1, "Go shopping.", false),
+            Task(2, "Go to work.", true)
+        )
+        objectMapper.writeValueAsString(tasks)
     }
 }
